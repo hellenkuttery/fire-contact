a47e31e2b6efb3adef2df0dc8862536631c839bf
hellenkuttery <helen@clarusway.com>
2021-12-23 23:28:58 +0300

    add allof files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import { useState } from 'react';
 import FormComponent from './components/form/FormComponent';
 import Contacts from './components/contacts/Contacts';
+import { addInfo, updateInfo } from './utils/functions';
+import { ToastContainer } from 'react-toastify';
 
 const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
 
@@ -9,13 +11,22 @@ function App() {
   const [info,setInfo]=useState(initialValues)
 
   const handleFormSubmit=()=>{
-    console.log(info)
+    if(info.id){
+    updateInfo(info)}
+    else{
+      addInfo(info)
+    }
   }
+
+  const editHandler=(id,username,phoneNumber,gender)=>{
+    setInfo({id,username,phoneNumber,gender});
+  };
 
   return (
     <div className="App">
      <FormComponent info={info} setInfo={setInfo} handleFormSubmit={handleFormSubmit} />
-     <Contacts/>
+     <Contacts editHandler={editHandler}/>
+     <ToastContainer />
     </div>
   );
 }
