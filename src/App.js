e17cfe660a768fb2efc0637ed552bef597bda6a7
hellenkuttery <helen@clarusway.com>
2021-12-23 20:47:52 +0300

    add form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+import { useState } from 'react';
 import FormComponent from './components/form/FormComponent';
 import Contacts from './components/contacts/Contacts';
 
+const initialValues={username:"",phoneNumber:"",gender:"NO INFO"}
+
 function App() {
+  const [info,setInfo]=useState(initialValues)
+
+  const handleFormSubmit=()=>{
+    console.log(info)
+  }
+
   return (
     <div className="App">
-     <FormComponent/>
+     <FormComponent info={info} setInfo={setInfo} handleFormSubmit={handleFormSubmit} />
      <Contacts/>
     </div>
   );
